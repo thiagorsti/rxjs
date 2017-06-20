8aa36f5c5046212815d2adc5ff420243b42d8674
Thiago Rodrigues <sistemas.thiago@gmail.com>
2017-06-20 20:17:17 -0300

    .

--- js/code1_4.js
+++ js/code1_4.js
@@ -1,44 +1,38 @@
-var quakes = Rx.Observable
-    .timer(0, 5000)
-    .flatMap(function() {        
-        return Rx.DOM.jsonpRequest({
-            url: QUAKE_URL,
-            jsonpCallback: 'eqfeed_callback'
-        }).retry(3);        
-    })
+var quakes = Rx.DOM.jsonpRequest({
+        url: QUAKE_URL,
+        jsonpCallback: 'eqfeed_callback'
+    }).retry(3)
     .flatMap(function transform(result) {
         return Rx.Observable.from(result.response.features);
-    })    
+    });
+
+var quakesData = Rx.Observable
+    .timer(1, 2000)
+    .flatMap(function(){        
+        return quakes;
+    })
     .map(function(quake){
         return {
             code: quake.properties.code,
             lat: quake.geometry.coordinates[1],
             lng: quake.geometry.coordinates[0],
             size: quake.properties.mag * 10000
-        };
+        }
     })
-    .distinct(function(quake) { 
+    .distinct(function(quake){
         return quake.code;
-    })
-
-var count = Rx.Observable
-    .timer(0, 10000)
-    .flatMap(function(){
-        return quakes.count();
     });
 
-// var marks = Rx.Observable
-//     .timer(0, 3000)
-//     .flatMap(function(){
-//         return quakes.distinct(function(quake) { 
-//             return quake.code;
-//         });
-//     });
+var quakesCount = Rx.Observable
+    .timer(0, 5000)
+    .flatMap(function(){
+        return quakes.count()
+    });
 
-count.subscribe(function(x){
-    console.log(x);
+quakesCount.subscribe(function(count){
+    console.log('Earthquakes: ' + count);
 });
 
-quakes.subscribe(function(quake) {
+quakesData.subscribe(function(quake) {
     L.circle([quake.lat, quake.lng], quake.size).addTo(map);
 });
